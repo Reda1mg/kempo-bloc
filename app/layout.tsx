e29dd8b6629f0cf303cdd5c737bfa3bc1c42ce7a
Reda1mg <mohamed.elalaoui@viacesi.fr>
2025-07-16 21:07:14 +0200

    added vercel analytics and speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,20 @@
 import './globals.css';
 import Sidebar from './components/Sidebar';
 
+import { Analytics } from "@vercel/analytics/next"
+
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <body className="flex min-h-screen bg-gradient-to-b from-[#181818] via-[#232526] to-[#181818] p-0 m-0">
         <Sidebar />
         <main className="flex-1 p-6 bg-[#222225] text-white min-h-screen flex flex-col">{children}</main>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
